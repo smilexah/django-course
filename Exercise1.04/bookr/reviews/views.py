184d21cd70e8e70655d6fb4f1177e1170932747e
Meiirzhan Muratbekuly <smilexah@inbox.ru>
2025-02-13 19:16:13 +0500

    exercise 1.04

---
+++ Exercise1.04/bookr/reviews/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def index(request):
+    name = request.GET.get("name") or "world"
+    return HttpResponse("Hello, {}!".format(name))

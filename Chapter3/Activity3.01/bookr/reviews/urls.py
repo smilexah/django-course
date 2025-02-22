812434aca4223cc7fc8ce14d6a5ba3edb6b8aa3b
S <kgr45637@mail.ru>
2025-02-22 16:07:28 +0500

    done chapter 3

--- Chapter3/Activity3.01/bookr/reviews/urls.py
+++ Chapter3/Activity3.01/bookr/reviews/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.welcome_view, name='welcome_view'),
+    path('', views.index, name='index'),
     path('books/', views.book_list, name='book_list'),
     path('books/<int:pk>/', views.book_detail, name='book_detail')
 ]
--- Chapter3/Activity3.01/bookr/reviews/views.py
+++ Chapter3/Activity3.01/bookr/reviews/views.py
@@ -6,7 +6,7 @@ from .utils import average_rating
 
 
 def index(request):
-    return render(request, "base.html")
+    return render(request, "reviews/base.html")
 
 
 def book_search(request):
